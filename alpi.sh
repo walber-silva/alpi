cd16725b813d1264a3ebb0ba507576e2e02cdc3c
Tiago R. Lampert <trepmalogait@gmail.com>
2016-01-13 16:25:26 -0200

    Update

--- alpi.sh
+++ alpi.sh
@@ -2,9 +2,9 @@
 ##################################################
 # Name: Arch Linux Pos Installation (alpi)
 # Description: Pos Installation for Arch Linux
-# Script Maintainer: ALT GNU/Linux
+# Script Maintainer: ALT Project
 #
-# Last Updated: 20/12/2015
+# Last Updated: 12/01/2016
 ##################################################
 #
 #Verificando se o pacman esta sendo usado por outro processo
@@ -19,19 +19,153 @@ fi
 
 pacman -Syu --noconfirm
 pacman -S --needed dialog --noconfirm
+#MULTIMIDIA
+multimidia(){
 
-program(){
+multimidia=$(
+      dialog --backtitle ' Arch Linux Pos Installation (alpi) - Version 0.6 Beta' \
+	     --stdout               \
+             --title 'Multimidia'  \
+             --menu 'Selecione o programa para instalar:' \
+            0 0 0                   \
+            1 'VLC' \
+	    2 'SMPlayer' \
+	    3 'Rhythmbox' \
+	    6 'Voltar' \
+            0 'Sair'                )
+
+	multimidia=$multimidia
+
+	[ $? -ne 0 ] && break
+
+	if [ "$multimidia" == "1" ]; then
+	clear
+	yaourt -S --needed vlc --noconfirm
+	multimidia
+
+	elif [ "$multimidia" == "2" ]; then
+	clear
+	yaourt -S --needed smplayer --noconfirm
+	multimidia
+
+	elif [ "$multimidia" == "3" ]; then
+	clear
+	yaourt -S --needed rhythmbox-git --noconfirm
+	multimidia
+
+	elif [ "$multimidia" == "6" ]; then
+	clear
+	programas
+else 
+echo 'Saindo do programa...'
+fi
+}
+menu
+
+#NAVEGADORES
+navegadores(){
+
+navegadores=$(
+      dialog --backtitle ' Arch Linux Pos Installation (alpi) - Version 0.6 Beta' \
+	     --stdout               \
+             --title 'Navegadores'  \
+             --menu 'Selecione o programa para instalar:' \
+            0 0 0                   \
+            1 'Mozilla Firefox' \
+	    2 'Google Chrome' \
+	    3 'Chromium' \
+	    4 'Iceweasel' \
+	    5 'Opera' \
+	    6 'Voltar' \
+            0 'Sair'                )
+
+	navegadores=$navegadores
+
+	[ $? -ne 0 ] && break
+
+	if [ "$navegadores" == "1" ]; then
+	clear
+	yaourt -S --needed firefox firefox gst-libav gst-plugins-good upower --noconfirm
+	navegadores
+
+	elif [ "$navegadores" == "2" ]; then
+	clear
+	yaourt -S google-chrome --noconfirm
+	navegadores
+
+	elif [ "$navegadores" == "3" ]; then
+	clear
+	yaourt -S --needed chromium --noconfirm
+	navegadores
+
+	elif [ "$navegadores" == "4" ]; then
+	clear
+	yaourt -S --needed iceweasel --noconfirm
+	navegadores
+
+	elif [ "$navegadores" == "5" ]; then
+	clear
+	yaourt -S --needed opera --noconfirm
+	navegadores
+
+	elif [ "$navegadores" == "6" ]; then
+	clear
+	programas
+else 
+echo 'Saindo do programa...'
+fi
+}
+menu
+
+#PROGRAMAS
+programas(){
+
+programas=$(
+      dialog --backtitle ' Arch Linux Pos Installation (alpi) - Version 0.6 Beta' \
+	     --stdout               \
+             --title 'Instalar Programas'  \
+             --menu 'Selecione a categoria:' \
+            0 0 0                   \
+            1 'Navegadores' \
+	    2 'Multimidia' \
+	    3 'Desenvolvimento' \
+	    9 'Voltar' \
+            0 'Sair'                )
+
+	programas=$programas
+
+	[ $? -ne 0 ] && break
+
+	if [ "$programas" == "1" ]; then
+	navegadores
+
+	elif [ "$programas" == "2" ]; then
+	multimidia
+
+	elif [ "$programas" == "3" ]; then
+	desenvolvimento
+
+	elif [ "$programas" == "9" ]; then
+	menu
+else 
+echo 'Saindo do programa...'
+fi
+}
+menu
+
+menu(){
 
 opcao=$(
-      dialog --backtitle ' Arch Linux Pos Installation (alpi) - Version 0.0.5 Beta' \
+      dialog --backtitle ' Arch Linux Pos Installation (alpi) - Version 0.6 Beta' \
 	     --stdout               \
              --title 'Menu'  \
              --menu 'Selecione a opcao:' \
             0 0 0                   \
-            1 'Instalacao base padrao (#ROOT)' \
+            1 'Instalacao padrao [PT-BR - GNOME] (#ROOT)' \
 	    2 'Instalacao completa padrao ($NO-ROOT)' \
-	    6 'Reiniciar'	\
-	    7 'Sobre'	\
+	    3 'Programas' \
+	    4 'Reiniciar'	\
+	    5 'Sobre'	\
             0 'Sair'                )
 
 	opcao=$opcao
@@ -66,7 +200,7 @@ opcao=$(
 	sh install.sh
 	cd ..
 	
-	program
+	menu
 
 	elif [ "$opcao" == "2" ]; then
 	clear
@@ -82,46 +216,44 @@ opcao=$(
 	sh installyaourt.sh
 	cd ..
 
-	program
+	menu
 
-	elif [ "$opcao" == "6" ]; then
+	elif [ "$opcao" == "3" ]; then
+	clear
+	programas
+
+	elif [ "$opcao" == "4" ]; then
 	reboot
 
-	program
+	menu
 
-	elif [ "$opcao" == "7" ]; then
+	elif [ "$opcao" == "5" ]; then
 	dialog --title 'Sobre' --msgbox '\n
 	\n
 	########################################################################\n
 	#                                                                      #\n
-	#       ALT GNU/Linux - Arch Linux Pos Installation (alpi)             #\n
+	#         ALT Project - Arch Linux Pos Installation (alpi)             #\n
 	#                                                                      #\n
 	########################################################################\n	
 	\n	
       	\n
 	Arch Linux Pos Instalacao (alpi), e uma ferramenta que permite facilitar a
-	configuracao do sistema Arch Linux apos a sua instalacao. Focada para iniciantes na 
-	distribuicao e para usuarios experientes que querem automatizar a tarefa de
+	configuracao do sistema Arch Linux apos a sua instalacao. Focada para iniciantes
+	na distribuicao e para usuarios experientes que querem automatizar a tarefa de
 	configuracao do sistema.\n
+	\n
+	\n
+	\n
+	\n
+	\n
+	                    Copyright (c) 2016 ALT Project\n
+	' 25 80
 
-      	\n
-	Arch Linux Post installation (alpi), is a tool that allows you to facilitate the
-	configuration of Arch Linux system after its installation. Focused to beginners in the 
-	distribution and for experienced users who want to automate the task of system 
-	configuration.\n
-	\n
-	\n
-	\n
-	\n
-	\n
-	                    Copyright (c) 2015 ALT GNU/Linux\n
-	' 30 80
-
-	program
+	menu
 
 
 else 
 echo 'Saindo do programa...'
 fi
 }
-program
+menu
